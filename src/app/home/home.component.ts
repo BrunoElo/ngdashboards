b04a616abbe2419afd5e5160430e67d62b17d868
bruno <elochux@gmail.com>
2021-04-03 12:57:31 +0100

    built home page for project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { dashboardList } from '../mocks/dashboards';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  dashboards = dashboardList;
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.dashboards);
   }
-
 }
---
+++ src/app/mocks/dashboards.ts
@@ -0,0 +1,12 @@
+export const dashboardList = [
+  {
+    name: 'Halion',
+    designer: 'Mike',
+    designerLink: 'https://www.twitter.com',
+    imgSrc: '../../assets/images/horse.png',
+    likes: 1,
+    rating: 1,
+  },
+  { name: '', designer: '', designerLink: '', imgSrc: '', likes: 1, rating: 1 },
+  { name: '', designer: '', designerLink: '', imgSrc: '', likes: 1, rating: 1 },
+];
